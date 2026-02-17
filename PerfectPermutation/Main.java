f64a932d5c4d34c6e6a7ed5783dd80ef97cc9388
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-02-17 14:44:16 +0530

    Completed Perfect permutation

---
+++ PerfectPermutation/Main.java
@@ -0,0 +1,19 @@
+package Codeforces.PerfectPermutation;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if (n % 2 != 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        for (int i = 1; i <= n; i += 2) {
+            System.out.print((i + 1) + " " + i + " ");
+        }
+
+    }
+}
