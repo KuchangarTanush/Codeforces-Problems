e72ca5d66539459a51963e0f531d1df865cd38ad
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-02-20 22:34:46 +0530

    Completed Easy Problem

---
+++ EasyProblem/Main.java
@@ -0,0 +1,14 @@
+// package Codeforces.EasyProblem;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        StringBuilder sb=new StringBuilder();
+        while(t-->0){
+            int n=sc.nextInt();
+            sb.append(n-1).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
