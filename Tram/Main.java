05f82f7ad072ac524856c7a603c7e338676b46e2
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-03-11 13:42:09 +0530

    Completed Tram

--- Tram/Main.java
+++ Tram/Main.java
@@ -1,5 +1,18 @@
-package Codeforces.Tram;
-
+// package Codeforces.Tram;
+import java.util.*;
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int capacity=0;
+        int max=0;
+        while(t-->0){
+            int out=sc.nextInt();
+            int in=sc.nextInt();
+            capacity+=in;
+            capacity-=out;
+            max=Math.max(capacity,max);
+        }
+        System.out.println(max);
+    }
 }
