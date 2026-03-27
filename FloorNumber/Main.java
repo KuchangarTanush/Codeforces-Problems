3fd5bb1f64b56035dd5b454c88ec3277705e546e
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-03-27 11:39:50 +0530

    Completed Floor Number

---
+++ FloorNumber/Main.java
@@ -0,0 +1,18 @@
+// package Codeforces.FloorNumber;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            if(n<=2){
+                System.out.println(1);
+            }else{
+                int floor=(n-3)/x+2;
+                System.out.println(floor);
+            }
+        }
+    }
+}
