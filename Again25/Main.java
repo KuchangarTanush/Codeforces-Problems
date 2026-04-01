8ac5f78583546f0e3d051a46b3d42dc8b99eb36d
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-04-01 07:16:42 +0530

    Completed

---
+++ Again25/Main.java
@@ -0,0 +1,12 @@
+// package Codeforces.Again25;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();  // n can be large
+        
+        // For all n >= 2, last two digits are always 25
+        System.out.println("25");
+    }
+}
