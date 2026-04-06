6f86e9e9513450fd4958daf310b67c43d444624c
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-04-06 11:14:43 +0530

    Completed

---
+++ PanoramixsPrediction/Main.java
@@ -0,0 +1,33 @@
+// package Codeforces.PanoramixsPrediction;
+import java.util.*;
+
+public class Main {
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = n + 1; i <= m; i++) {
+            if (isPrime(i)) {
+                if (i == m) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+                return; // important: stop after first prime
+            }
+        }
+
+        // if no prime found at all
+        System.out.println("NO");
+    }
+}
