25e033a8ab499dabae27a44a762e68af09dfb88e
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-04-09 17:41:40 +0530

    Completed

---
+++ Borze/Main.java
@@ -0,0 +1,27 @@
+// package Codeforces.Borze;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); ) {
+            if (s.charAt(i) == '.') {
+                result.append('0');
+                i++;
+            } else {
+                if (s.charAt(i + 1) == '.') {
+                    result.append('1');
+                } else {
+                    result.append('2');
+                }
+                i += 2;
+            }
+        }
+        
+        System.out.println(result.toString());
+    }
+}
