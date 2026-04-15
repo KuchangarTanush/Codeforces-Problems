5a24d3c1ff6c2d99ff1aa908b3bef9c2e7b5cb0b
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-04-15 07:18:16 +0530

    Completed

---
+++ GeorgeAndAccomodation/Main.java
@@ -0,0 +1,22 @@
+// package Codeforces.GeorgeAndAccomodation;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+            if (q - p >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
