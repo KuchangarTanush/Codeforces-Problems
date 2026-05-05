cdd1ccea9f7aed1d2ccbe0708202a539ca89c4f6
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-05-05 15:24:05 +0530

    Completed

---
+++ ILoveUsername/Main.java
@@ -0,0 +1,25 @@
+// package Codeforces.ILoveUsername;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=arr[0];
+        int min=arr[0];
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                cnt++;
+                max=arr[i];
+            }else if(arr[i]<min){
+                cnt++;
+                min=arr[i];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
