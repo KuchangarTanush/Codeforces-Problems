59c6a8419e64fe421ad33c651023e7dba41e3be3
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-05-06 12:40:52 +0530

    Completed

---
+++ SystemOfEquations/Main.java
@@ -0,0 +1,17 @@
+// package Codeforces.SystemOfEquations;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int count=0;
+        for(int a=0;a*a<=n;a++){
+            int b=n-(a*a);
+            if(b>=0 && (a+(b*b)==m)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
