6b982e242230892a931463ccf29ab94cc3337f18
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-05-18 10:08:15 +0530

    Completed

---
+++ LittleNikita/Main.java
@@ -0,0 +1,17 @@
+// package Codeforces.LittleNikita;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            if(n>=m&& (n - m) % 2 == 0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
