f59b6f117fe4ba27371641057cad6f19664c9cf7
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-05-22 12:52:02 +0530

    Completed

---
+++ CloudberryJam/Main.java
@@ -0,0 +1,12 @@
+// package Codeforces.CloudberryJam;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-->0){
+            long n=sc.nextLong();
+            System.out.println(2*n);
+        }
+    }
+}
