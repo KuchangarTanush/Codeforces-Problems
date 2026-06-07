420d389b1e6b2446186c75fcbf000c3a3a8dcf0c
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-06-07 12:39:41 +0530

    Completed

---
+++ Lever/Main.java
@@ -0,0 +1,40 @@
+// package Codeforces.Lever;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+
+            int extra = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (a[i] > b[i]) {
+                    extra += a[i] - b[i];
+                }
+            }
+
+            if (extra == 0)
+                System.out.println(1);
+            else
+                System.out.println(extra + 1);
+        }
+
+        sc.close();
+    }
+}
