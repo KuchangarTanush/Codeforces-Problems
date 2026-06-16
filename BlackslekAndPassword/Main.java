b918b252c976f12ef7d115b96547cc88ca3c12e9
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-06-16 20:38:24 +0530

    Completed

---
+++ BlackslekAndPassword/Main.java
@@ -0,0 +1,14 @@
+// package Codeforces.BlackslekAndPassword;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int k=sc.nextInt();
+            int x=sc.nextInt();
+            int ans=k*x+1;
+            System.out.println(ans);
+        }
+    }
+}
