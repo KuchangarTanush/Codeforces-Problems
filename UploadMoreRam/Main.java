20fefdb34a1833bc39c5d6b86a13baceca426e9e
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-06-30 14:55:20 +0530

    Completed

---
+++ UploadMoreRam/Main.java
@@ -0,0 +1,14 @@
+// package Codeforces.UploadMoreRam;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int ans=1+(n-1)*k;
+            System.out.println(ans);
+        }
+    }
+}
