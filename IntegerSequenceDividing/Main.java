67fb5c82f599d5123b6c8f0287477a53b752dd5b
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-07-04 11:27:35 +0530

    Completed

---
+++ IntegerSequenceDividing/Main.java
@@ -0,0 +1,13 @@
+// package Codeforces.IntegerSequenceDividing;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n = sc.nextLong();
+
+        if (n % 4 == 0 || n % 4 == 3)
+            System.out.println(0);
+        else
+            System.out.println(1);
+    }
+}
