72c8017b3158a32d0d7cf54033ba2225415168dc
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-07-07 21:03:42 +0530

    Completed

---
+++ MezoPlayingZoma/Main.java
@@ -0,0 +1,13 @@
+// package Codeforces.MezoPlayingZoma;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.next(); // Read the command string (not actually needed)
+
+        System.out.println(n + 1);
+    }
+}
