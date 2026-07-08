9a84de302361c4c803093d38841172df9b86ace4
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-07-08 19:51:33 +0530

    Completed

---
+++ TooMinTooMax/Main.java
@@ -0,0 +1,25 @@
+// package Codeforces.TooMinTooMax;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            Arrays.sort(a);
+
+            long ans = 2L * ((long)a[n - 1] + a[n - 2] - a[0] - a[1]);
+
+            System.out.println(ans);
+        }
+    }
+}
