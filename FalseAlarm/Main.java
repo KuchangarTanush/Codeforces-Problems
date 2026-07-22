c48fad58f52ecdaf815d32aaa10eb2829c564f88
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-07-22 11:37:15 +0530

    Completed

---
+++ FalseAlarm/Main.java
@@ -0,0 +1,26 @@
+// package Codeforces.FalseAlarm;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int first=1,last=-1;
+            for(int i=0;i<n;i++){
+                int a=sc.nextInt();
+                if(a==1){
+                    if(first==1)first=i;
+                    last=i;
+                }
+            }
+            if(last-first+1<=x){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+
+    }
+}
