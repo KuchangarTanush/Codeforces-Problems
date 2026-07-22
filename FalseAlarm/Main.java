cf5fb4f59593663436dd77f0b7f3ba27b7f84697
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-07-22 11:40:11 +0530

    Completed

--- FalseAlarm/Main.java
+++ FalseAlarm/Main.java
@@ -7,19 +7,22 @@ public class Main {
         while(t-->0){
             int n=sc.nextInt();
             int x=sc.nextInt();
-            int first=1,last=-1;
-            for(int i=0;i<n;i++){
-                int a=sc.nextInt();
-                if(a==1){
-                    if(first==1)first=i;
-                    last=i;
-                }
-            }
-            if(last-first+1<=x){
-                System.out.println("Yes");
-            }else{
-                System.out.println("No");
-            }
+            int first = -1, last = -1;
+
+for (int i = 0; i < n; i++) {
+    int a = sc.nextInt();
+
+    if (a == 1) {
+        if (first == -1)
+            first = i;
+        last = i;
+    }
+}
+
+if (last - first + 1 <= x)
+    System.out.println("YES");
+else
+    System.out.println("NO");
         }
 
     }
