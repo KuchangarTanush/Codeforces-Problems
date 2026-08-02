474811cf958b49250a0497a9914433c1706e90d9
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-08-02 13:40:30 +0530

    Completed

---
+++ LineBreaks/Main.java
@@ -0,0 +1,32 @@
+// package Codeforces.LineBreaks;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            int sum = 0;
+            int count = 0;
+            boolean stop = false;
+
+            for (int i = 0; i < n; i++) {
+                String s = sc.next();
+
+                if (!stop && sum + s.length() <= m) {
+                    sum += s.length();
+                    count++;
+                } else {
+                    stop = true;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}
