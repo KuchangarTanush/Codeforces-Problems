7c470f49e3b3d5ee94c8094e126b1635fddc49db
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-08-09 11:17:30 +0530

    Completed

---
+++ TwoOr3Moves/Main.java
@@ -0,0 +1,17 @@
+// package Codeforces.TwoOr3Moves;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            if(n==1){
+                System.out.println(2);
+            }else{
+                System.out.println((n + 2) / 3);
+            }
+        }
+
+    }
+}
