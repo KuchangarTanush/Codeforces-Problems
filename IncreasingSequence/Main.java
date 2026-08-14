2b7faa3a6dbde84a9c84c450c0515e17844fdbc5
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-08-14 11:44:33 +0530

    Completed

---
+++ IncreasingSequence/Main.java
@@ -0,0 +1,20 @@
+// package Codeforces.IncreasingSequence;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int curr=0;
+            for(int i=0;i<n;i++){
+                int a=sc.nextInt();
+                curr++;
+                if(curr==a){
+                    curr++;
+                }
+            }
+            System.out.println(curr);
+        }
+    }
+}
