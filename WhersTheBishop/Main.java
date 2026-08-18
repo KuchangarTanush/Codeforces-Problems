b7bd24ad6cadb9f69bf6ac554235d6dc461dad55
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-08-18 15:34:56 +0530

    Completed

---
+++ WhersTheBishop/Main.java
@@ -0,0 +1,38 @@
+// package Codeforces.WhersTheBishop;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            char[][] board = new char[8][8];
+
+            for (int i = 0; i < 8; i++) {
+                board[i] = sc.next().toCharArray();
+            }
+
+            for (int i = 1; i < 7; i++) {
+                for (int j = 1; j < 7; j++) {
+
+                    if (board[i][j] == '#' &&
+                        board[i - 1][j - 1] == '#' &&
+                        board[i - 1][j + 1] == '#' &&
+                        board[i + 1][j - 1] == '#' &&
+                        board[i + 1][j + 1] == '#') {
+
+                        System.out.println((i + 1) + " " + (j + 1));
+                    }
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
