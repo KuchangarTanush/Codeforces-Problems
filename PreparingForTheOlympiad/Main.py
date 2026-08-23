cd1871606c8531fa9b6132c3c53239d54777bd23
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-08-23 13:33:55 +0530

    Completed

---
+++ PreparingForTheOlympiad/Main.py
@@ -0,0 +1,10 @@
+#preparing for the olympiad
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    ans=a[n-1]
+    for i in range(n-1):
+        ans+=max(0,a[i]-b[i+1])
+    print(ans)
