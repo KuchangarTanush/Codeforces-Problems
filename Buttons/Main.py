f98a5ef11e9f89b540644e8cc98c99d93ef62466
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-09-16 14:01:25 +0530

    Completed

---
+++ Buttons/Main.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    if a > b:
+        print("First")
+    elif a < b:
+        print("Second")
+    else:
+        if c % 2 == 1:
+            print("First")
+        else:
+            print("Second")
