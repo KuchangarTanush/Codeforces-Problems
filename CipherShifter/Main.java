f43c33ce868782ad890074031a46892595df2bcb
Kuchangar Tanush <tanushkuchangar@gmail.com>
2026-09-24 14:43:24 +0530

    Completed

---
+++ CipherShifter/Main.java
@@ -0,0 +1,24 @@
+// package Codeforces.CipherShifter;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            String s=sc.next();
+            StringBuilder sb=new StringBuilder();
+            int i=0;
+            while(i<n){
+                char ch=s.charAt(i);
+                sb.append(ch);
+                int j=i+1;
+                while (s.charAt(j)!=ch) {
+                    j++;
+                }
+                i=j+1;
+            }
+            System.out.println(sb);
+        }
+    }
+}
